c64c7b2974e8293dccc9e262a36580e7b524c9ce
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-03 21:01:20 +0100

    feat: chapter 12 (inline views & widgets)

--- addons/custom/estate/models/estate_property_type.py
+++ addons/custom/estate/models/estate_property_type.py
@@ -1,9 +1,12 @@
-from odoo import models,fields
+from odoo import models, fields
+
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
 
     name = fields.Char(required=True)
+
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'Tag must be unique')
