29a3a7beb032c74e6dd1dc81fc6af76db7435f23
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-05 18:51:20 +0100

    feat: chapter 13 (Model and view Inheritance)

--- addons/custom/estate/__manifest__.py
+++ addons/custom/estate/__manifest__.py
@@ -6,6 +6,7 @@
     'application': True,
     'data': [
     'data/ir.model.access.csv',
+    'views/inherited_res_users_views.xml',
     'views/estate_property_views.xml',
     'views/estate_property_offer_views.xml',
     'views/estate_property_type_views.xml',
