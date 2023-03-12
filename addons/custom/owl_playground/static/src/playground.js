f7e7e205b5671526d85e37ebe21455f86c03831d
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-12 12:02:13 +0100

    feat: Add todo component and props validation (Chapter 1.3-4)

--- addons/custom/owl_playground/static/src/playground.js
+++ addons/custom/owl_playground/static/src/playground.js
@@ -6,6 +6,10 @@ import {Todo} from './todo'
 
 export class Playground extends Component {
     static template = "owl_playground.playground"
-
     static components = { Counter, Todo }
+
+    setup(){
+        this.todos = [{id: 1, description: "Milk", done: true}, {id: 2, description: "Strawberry", done: false}]
+    }
+
 }
--- addons/custom/owl_playground/static/src/todo.js
+++ addons/custom/owl_playground/static/src/todo.js
@@ -1,26 +1,22 @@
 /** @odoo-module **/
 import {Component, useState, xml } from "@odoo/owl";
 export class Todo extends Component {
-    static defaultProps = {
-        milk: {
-            id: 1,
-            description: 'Milk',
-            done: true
-        }
-    }
+    // static defaultProps = {
+    //         id: 1,
+    //         description: 'Milk',
+    //         done: true
+    // }
 
     static props = {
-        milk: {
-            id: Number,
-            description: String,
-            done: Boolean
-        }
+        id: Number,
+        description: String,
+        done: Boolean
     }
 
     static template = xml`
     <div>
-        <div t-att-class="{'text-muted text-decoration-line-through': props.milk.done }">
-            <span t-esc="props.milk.id"/> . <span t-esc="props.milk.description" />
+        <div t-att-class="{'text-muted text-decoration-line-through': props.done }">
+            <span t-esc="props.id"/> . <span t-esc="props.description" />
         </div>
     </div>
     `
