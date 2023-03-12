617ba41ec9e0b4591e445742e87295ed3ce70499
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-12 17:33:10 +0100

    feat: Toggling todos (Chapter 7)

--- addons/custom/owl_playground/static/src/playground.js
+++ addons/custom/owl_playground/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useState, useEffect } from "@odoo/owl";
+import { Component, useState, onMounted, useRef } from "@odoo/owl";
 import {Counter} from './counter'
 import {Todo} from './todo'
 
@@ -9,6 +9,21 @@ export class Playground extends Component {
     setup(){
         this.todos = useState([{id: 1, description: "Milk", done: true}, {id: 2, description: "Strawberry", done: false}])
         this.id= this.todos[this.todos.length-1].id
+
+        const inputRef = useRef("inputRef")
+        onMounted(() => {
+            inputRef.el && inputRef.el.focus()
+        })
+    }
+
+    toggleState(event,id){
+      const todos = this.todos.map(todo => {
+          if(todo.id === id) {
+            todo.done = event.currentTarget.checked
+      }
+          return todo
+      })
+        this.todos = todos
     }
 
     addTodo(event){
--- addons/custom/owl_playground/static/src/todo.js
+++ addons/custom/owl_playground/static/src/todo.js
@@ -1,22 +1,19 @@
 /** @odoo-module **/
 import {Component, useState, xml } from "@odoo/owl";
 export class Todo extends Component {
-    // static defaultProps = {
-    //         id: 1,
-    //         description: 'Milk',
-    //         done: true
-    // }
 
     static props = {
         id: Number,
         description: String,
-        done: Boolean
+        done: Boolean,
+        toggleState: Function
     }
 
     static template = xml`
     <div>
         <div t-att-class="{'text-muted text-decoration-line-through': props.done }">
             <span t-esc="props.id"/> . <span t-esc="props.description" />
+            <input type="checkbox" t-model="props.done" t-on-click="(event) => props.toggleState(event,props.id)"/>
         </div>
     </div>
     `
