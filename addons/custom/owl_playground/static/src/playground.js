85bb4caea72cedf0c12bb44b76e72afa73714e28
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-12 12:46:16 +0100

    feat: Add todo component and props validation (Chapter 6)

--- addons/custom/owl_playground/static/src/playground.js
+++ addons/custom/owl_playground/static/src/playground.js
@@ -1,15 +1,28 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState, useEffect } from "@odoo/owl";
 import {Counter} from './counter'
 import {Todo} from './todo'
 
 export class Playground extends Component {
-    static template = "owl_playground.playground"
     static components = { Counter, Todo }
-
     setup(){
-        this.todos = [{id: 1, description: "Milk", done: true}, {id: 2, description: "Strawberry", done: false}]
+        this.todos = useState([{id: 1, description: "Milk", done: true}, {id: 2, description: "Strawberry", done: false}])
+        this.id= this.todos[this.todos.length-1].id
     }
 
+    addTodo(event){
+        if(event.keyCode === 13 && event.currentTarget.value !== ""){
+            this.todos.push(
+                {
+                    id: this.id +=1,
+                    description: event.currentTarget.value,
+                    done:false
+                }
+            )
+            event.currentTarget.value = ""
+        }
+    }
+
+    static template = "owl_playground.playground"
 }
