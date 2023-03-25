90263d1095df950b2da9074c124f6a914c1f1ea2
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2023-03-25 12:10:29 +0100

    feat: Define module data (CSV, XML -> Ref)

--- addons/custom/awesome_tshirt/static/src/dashboard.js
+++ addons/custom/awesome_tshirt/static/src/dashboard.js
@@ -4,7 +4,10 @@ import { registry } from "@web/core/registry";
 import {Layout} from '@web/search/layout';
 import { getDefaultConfig } from "@web/views/view";
 
-const { Component, useSubEnv } = owl;
+const { Component, useSubEnv, onWillStart } = owl;
+import { useService } from "@web/core/utils/hooks";
+import { Domain } from '@web/core/domain';
+
 
 class AwesomeDashboard extends Component {
 
@@ -18,6 +21,35 @@ class AwesomeDashboard extends Component {
         this.display = {
             controlPanel: { "top-right": false, "bottom-right": false }
         }
+
+        this.action = useService('action')
+
+        this.rpc = useService("rpc")
+        onWillStart(async () => {
+            const result = await this.rpc('/awesome_tshirt/statistics')
+            console.log('result')
+        })
+    }
+
+    openCustomers(){
+        this.action.doAction("base.action_partner_form")
+    }
+
+    newOrders(){
+    }
+    cancelOrders(){
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state', '=', 'cancelled')]";
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: 'Last 7 days orders',
+            res_model: "awesome_tshirt.order",
+            domain: new Domain(domain).toList(),
+            views: [
+                [false, 'list'],
+                [false, 'form']
+            ]
+        })
     }
 }
 
--- addons/custom/estate/__manifest__.py
+++ addons/custom/estate/__manifest__.py
@@ -5,6 +5,8 @@
     'installable': True,
     'application': True,
     'data': [
+    'data/estate.property.type.csv',
+    'data/demo_data.xml',
     'data/ir.model.access.csv',
     'views/inherited_res_users_views.xml',
     'views/estate_property_views.xml',
